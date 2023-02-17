d9261533df7cb46b15cee799acde054197be25fd
ivanpp <ivanding94@gmail.com>
2023-02-17 13:34:13 +0100

    Add script, readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # arm4stencil
+
+## Use gem5
+
+```bash
+# with 2level-arm.py
+#   bin is defined in 2level-arm.py, modify it first
+sh scripts/sim.sh OUT_DIR
+
+# with se.py
+sh scripts/sim-sve.sh OUT_DIR BIN VEC
+```
+
+## Techinique explain
+
+* [loop unrolling](https://en.wikipedia.org/wiki/Loop_unrolling)
+* [Loop fission and fusion](https://en.wikipedia.org/wiki/Loop_fission_and_fusion)
+* [Blocking (Loop nest optimization)](https://en.wikipedia.org/wiki/Loop_nest_optimization)
---
+++ gem5/scripts/sim-sve.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+OUTPUT_DIR=$1
+BIN=$2
+VEC=$3
+M5OUT_DIR="m5out"
+BASH_OUT=$M5OUT_DIR/$OUTPUT_DIR/output.txt
+CONFIG_DIR="configs"
+MODEL_NAME="se"
+BIN_DIR="../stencil-3d/bin"
+
+GEM5_BIN="build/ARM/gem5.opt"
+
+if [ $# -eq 0 ]
+then
+    >&2 echo "usage: srcipts/sim.sh OUTPUT_DIR BIN VEC"
+    >&2 echo "VEC should be 1(128)/2(256)/4(512)/8(1024)/16(2048)"
+    exit 1
+else
+    echo "------------------------------------"
+    echo "GEM5 ARM syscall emulation"
+    echo
+    echo "model: $CONFIG_DIR/$MODEL_NAME.py"
+    echo "m5out: $M5OUT_DIR/$OUTPUT_DIR"
+    echo "bin:  $BIN_DIR/$BIN"
+    echo "------------------------------------"
+fi
+
+# syscall emulation, also save bash output
+$GEM5_BIN -d "$M5OUT_DIR/$OUTPUT_DIR" "$CONFIG_DIR/$MODEL_NAME.py" \
+--param "system.cpu[:].isa[:].sve_vl_se = $VEC" --cpu-type=ex5_big \
+--caches -c "$BIN_DIR/$BIN" | tee $BASH_OUT
+# copy the configuration py file too
+cp "$CONFIG_DIR/$MODEL_NAME.py" "$M5OUT_DIR/$OUTPUT_DIR/"
